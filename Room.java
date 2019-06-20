22211474c7c66683a9a3e58f1dfc289f1c6bea96
michelle-prog <michellemawuena@gmail.com>
2019-06-20 03:51:20 +0000

    Work

---
+++ Room.java
@@ -0,0 +1,63 @@
+class Room{
+	private double width;
+	private double length;
+	private int floor;
+
+
+public Room(){
+double width = 10.0;
+double length = 12.5;
+int floor = 1;	
+
+public double getWidth(){
+	return width;
+}
+
+public void setWidth(double newWidth){
+if(newWidth <= 0){
+	
+}
+else{
+	this.width = newWidth:
+}
+
+public double getLength(){
+	return length;
+}
+public void setLength(double newLength){
+	if(newLength <= 0){
+
+	}
+	else{
+	    this.length = newLength;
+	}
+
+	public int getFloor(){
+	return floor;
+	}
+	public void setFloor(int newFloor){
+	this.name = newFloor;
+	}
+
+	public Room(double length,double width,int floor){
+	double length = length;
+	double width = width;
+	int floor =floor;
+	}
+	public static void main(String[] args){
+	Room myRoom = new Room(12.5,10.0,1);
+	System.out.println(myRoom.double length + myRoom.double width + myRoom.int floor);
+	}
+}
+
+public String toString(){
+	return "length * width on floor floor";
+}
+
+public int Compareto(Room){
+	(floor,length,width);
+}
+}
+
+
+
